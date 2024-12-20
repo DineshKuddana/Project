d9a16225d78870d2a1fe6dea283d8dec4fc615cf
DineshKuddana <dineshkuddana@gmail.com>
2024-12-20 10:48:47 +0530

    merged

--- app/components/Dashboard/Sidebar.js
+++ app/components/Dashboard/Sidebar.js
@@ -64,6 +64,8 @@ const routes = [
 function Sidebar() {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+
+
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-12 h-12 mr-4">
@@ -90,6 +92,7 @@ function Sidebar() {
           ))}
         </div>
       </div>
+
     </div>
   );
 }
